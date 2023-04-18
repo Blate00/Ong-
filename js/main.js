b78cff48737b35daca6f50f44fb716bfcc659c32
Blate00 <da.coo@duocuc.cl>
2023-04-18 13:50:09 -0400

    nico

--- js/main.js
+++ js/main.js
@@ -25,6 +25,18 @@ function cambiarColorImagen(idImagen, nuevoColor) {
     var resultado = num1 + num2;
     document.getElementById("resultado").innerHTML = resultado;}
 
-    
+    function validar() {
+      var email = document.getElementById("email").value;
+      if (validarEmail(email)) {
+        document.getElementById("mensaje").innerHTML = "La dirección de correo electrónico es válida.";
+      } else {
+        document.getElementById("mensaje").innerHTML = "La dirección de correo electrónico no es válida.";
+      }
+    }
+
+    function validarEmail(email) {
+      var expresion = /^[^\s()<>,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+      return expresion.test(email);
+    }
 
 
