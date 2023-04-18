2052b97575427f5b04560e4fa16226d10b7a4a80
Blate00 <127876574+Blate00@users.noreply.github.com>
2023-04-18 13:22:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function cambiarContenido(){
+    return "Que pasa coxino culiao"
+}
+
+function Sumar(a,b){
+var a=10;
+var b=10;
+var c = a+b;
+return c
+}
+
+window.alert("Bienvenido coxino conchetumare en esta wea vamo a traficar chihuahuas entero cornotao xorooooo");
+
+function cambiarColorImagen(idImagen, nuevoColor) {
+    const imagen = document.getElementById(idImagen);
+    imagen.style.filter = `hue-rotate(${nuevoColor}deg)`;
+  }
+  
+  function sumar() {
+
+    var num1 = document.getElementById("numero1").value;
+    var num2 = document.getElementById("numero2").value;
+    num1 = parseInt(num1);
+    num2 = parseInt(num2); 
+    var resultado = num1 + num2;
+    document.getElementById("resultado").innerHTML = resultado;}
+
+    
+
+
